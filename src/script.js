df567a76f1ed129b4028c943360914532fb58dd3
Javier <jandresch017@gmail.com>
2023-05-16 22:48:00 -0600

    creación de carpeta para distintas vistas de página

---
+++ src/script.js
@@ -0,0 +1,10 @@
+console.log("si me cargo");
+
+const loginBtn = document.getElementById('login');
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(true){
+        location.href="../pages/main.html";
+    }
+});
